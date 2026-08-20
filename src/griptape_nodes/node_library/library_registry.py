89467d7c037b3cf3258f0a495066438f08e0dcfc
SibiSivarajan <31584854+SibiSivarajan@users.noreply.github.com>
2026-08-20 16:42:01 +0100

    Update library_registry.py

--- src/griptape_nodes/node_library/library_registry.py
+++ src/griptape_nodes/node_library/library_registry.py
@@ -370,6 +370,8 @@ class LibraryRegistry:
     @classmethod
     def get_library(cls, name: str) -> Library:
         if name not in cls._libraries:
+            registered = list(cls._libraries.keys())
+            logger.error("Library '%s' not found. Registered libraries: %s", name, registered)
             msg = f"Library '{name}' not found"
             raise LibraryRegistryError(msg)
         return cls._libraries[name]
