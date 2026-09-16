18904927e489ca9ad25c6f8e4c1efd55fe3cc83d
Collin Dutter <collin@griptape.ai>
2026-09-16 15:50:55 -0700

    refactor(traits): declare AddParameterButton's type as a class constant

--- src/griptape_nodes/traits/add_param_button.py
+++ src/griptape_nodes/traits/add_param_button.py
@@ -1,3 +1,5 @@
+from typing import ClassVar
+
 import attrs
 
 from griptape_nodes.exe_types.core_types import WIRING, Trait, default_element_id
@@ -5,11 +7,12 @@ from griptape_nodes.traits.button import Button
 
 
 class AddParameterButton(Trait):
+    type: ClassVar[str] = "AddParameter"
+
     element_id: str = attrs.field(default="AddParameterButton", converter=default_element_id, metadata=WIRING)
 
     def __attrs_post_init__(self) -> None:
         super().__attrs_post_init__()
-        self.type = "AddParameter"
         self.add_child(Button(label="AddParameter"))
 
     def ui_options_for_trait(self) -> dict:
